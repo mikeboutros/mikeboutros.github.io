2a698947db9cb86272058729569890421b87b7cd
mb <40812568+mikeboutros@users.noreply.github.com>
2021-03-10 00:42:02 -0800

    Updates

---
+++ client/List6.7088ee99.js
@@ -0,0 +1,1 @@
+import{S as s,i as t,s as l}from"./client.1dfc8c16.js";export default class extends s{constructor(s){super(),t(this,s,null,null,l,{})}}
--- src/routes/tech-skills/ListItem.svelte
+++
@@ -1,6 +0,0 @@
-<script>
-    export let item;
-    let src = item;
-</script>
-
-<img {src} alt={item} />
